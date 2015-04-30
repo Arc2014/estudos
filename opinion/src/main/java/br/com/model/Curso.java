95612d8ffd9e3c87adf44c933bb2947309e104ad
aurelio.costa <aurelio.costa@afferolab.com.br>
2015-04-30 16:53:16 -0300

    Rest funcionando com Json

---
+++ opinion/src/main/java/br/com/model/Curso.java
@@ -0,0 +1,48 @@
+package br.com.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Date;
+
+/**
+ * Created by arcosta on 30/04/15.
+ */
+
+@XmlRootElement
+public class Curso {
+
+    private String nome;
+    private String descricao;
+    private Date dataCriacao;
+
+    public Curso(){ };
+
+    public Curso(String nome, String descricao, Date dataCriacao) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.dataCriacao = dataCriacao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+}
--- opinion/src/main/java/br/com/rest/service/FormService.java
+++ opinion/src/main/java/br/com/rest/service/FormService.java
@@ -1,21 +1,28 @@
 package br.com.rest.service;
 
+import br.com.model.Curso;
+import com.google.gson.Gson;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Date;
 
 /**
  * Created by arcosta on 30/04/15.
  */
-@Path(value = "/form")
+@Path("/form")
 public class FormService {
 
     @GET
-    @Path(value = "/{param}")
-    public Response getMessage(@PathParam(value="param") String message) {
-        String output = "Jersey says " + message;
-        return Response.status(200).entity(output).build();
+    @Path("/{param}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getCurso(@PathParam(value="param") String nome) {
+        Curso curso = new Curso(nome, "Primeiro curso criado", new Date());
+        return Response.status(Response.Status.OK).entity(new Gson().toJson(curso)).build();
     }
 }
 
