cf13a08f255cb0c643448305881fea368ae2f0e6
Arc2014 <aureliorezendecosta@hotmail.com>
2015-05-04 16:14:45 -0300

    Update db_opinion.sql

--- db_opinion.sql
+++ db_opinion.sql
@@ -3,19 +3,19 @@ create database opinion;
 use opinion;
 
 create table tb_curso(
-	id int primary key auto_increment,
+    id int primary key auto_increment,
     nome varchar(100)
     
 );
 
 create table tb_pergunta(
-	id int primary key auto_increment,
+    id int primary key auto_increment,
     descricao varchar(1000)
-
+    tipo varchar(10)
 );
 
 create table tb_pergunta_curso(
-	id int primary key auto_increment,
+    id int primary key auto_increment,
     resposta varchar(1000),
     id_curso int,
     id_pergunta int,
