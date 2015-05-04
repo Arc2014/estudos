883a5db94607603fd148106132266919b0e5f226
Arc2014 <aureliorezendecosta@hotmail.com>
2015-05-04 16:01:42 -0300

    Create db_opinion.sql
    script database

---
+++ db_opinion.sql
@@ -0,0 +1,25 @@
+create database opinion;
+
+use opinion;
+
+create table tb_curso(
+	id int primary key auto_increment,
+    nome varchar(100)
+    
+);
+
+create table tb_pergunta(
+	id int primary key auto_increment,
+    descricao varchar(1000)
+
+);
+
+create table tb_pergunta_curso(
+	id int primary key auto_increment,
+    resposta varchar(1000),
+    id_curso int,
+    id_pergunta int,
+    
+    constraint FK_CURSO foreign key (id_curso) references tb_curso(id),
+    constraint FK_PERGUNTA foreign key (id_pergunta) references tb_pergunta(id)
+);
